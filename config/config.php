ffb776df401fb0787469257711fe4ba0b2b4e8b9
player259 <>
2021-11-15 10:22:54 +0200

    Drop hydration result caching

--- config/config.php
+++ config/config.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Contracts\Cache\CacheInterface;
 use Symplify\PackageBuilder\Strings\StringFormatConverter;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -17,9 +15,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Symplify\EasyHydrator\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/EasyHydratorBundle.php']);
-
-    $services->set(FilesystemAdapter::class);
-    $services->alias(CacheInterface::class, FilesystemAdapter::class);
 
     $services->set(StringFormatConverter::class);
 };
